a9c7e1c0fdd6fa42118dd66baf6219f2df8cc23b
DarthVader1507 <ashwath.chandramouli06@gmail.com>
2026-01-14 17:44:04 +0530

    Victory Screen added

--- Platformer/Assets/Scripts/VictoryScript.cs
+++ Platformer/Assets/Scripts/VictoryScript.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class VictoryScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private DeathScript deathScript;
+    [SerializeField] private GameObject victoryPanel;
     void Start()
     {
-        
+        deathScript = FindObjectOfType<DeathScript>();
+        victoryPanel.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player") && deathScript.isDead == false)
+        {
+            Debug.Log("Victory!");
+            Time.timeScale = 0;
+            victoryPanel.SetActive(true);
+        }
     }
 }
